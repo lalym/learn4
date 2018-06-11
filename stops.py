546919dfef465dd277708cc018a7a9d6ddac1565
Alex Lalym <alexander.lalym@gmail.com>
2018-06-11 22:17:13 +0300

    версия 2 финал

--- stops.py
+++ stops.py
@@ -7,12 +7,12 @@ with open('stops.csv', 'r', encoding='cp1251') as file:
     for row in reader:
         counter.setdefault(row['Street'], 0)
         counter[row['Street']] += 1
-    x = sum(counter.values())  # summa vsex ostanovok
+    x = sum(counter.values())  # sum stops
     print("Кол-во остановок: {}".format(x))
 
-    v = list(counter.values())  # list iz kluchey
-    k = list(counter.keys())  # list iz znacheniy
-    max_k = k[v.index(max(v))]  # ulica
+    v = list(counter.values())  # values list
+    k = list(counter.keys())  # keys list
+    max_k = k[v.index(max(v))]  # street
     print("Улица, на которой больше всего остановок: {}".format(max_k))
-    max_v = v[v.index(max(v))]  # ostanovki
+    max_v = v[v.index(max(v))]  # stops
     print("Количество остановок на улице {} : {}".format(max_k, max_v))
